cfbfaf137e92a4b3f805eb66c50a050e5a28c715
Sh1neSp4rk <31965386+Sh1neSp4rk@users.noreply.github.com>
2024-08-09 02:36:56 -0400

    Create code.ino
    initial commit

---
+++ code.ino
@@ -0,0 +1,38 @@
+int inPins[9] ={0,1,2,3,4,5,6,7,8}; //set the pins for directional input
+int outPinsStandard[8] ={8,9,10,A0,A1,A2,A3,A4}; //set output pins for direction
+int outPinsGoofy[8] ={A1,A2,A3,A4,8,9,10,A0}; //sets flipped outputs when switch is on
+int switchPin = A5; //set the switch pin
+int pinState;
+  
+void setup()
+{
+  //set all the appropriate pins to input
+ for (int i=0; i < 7; ++i) {
+    pinMode(inPins[i],INPUT);
+    digitalWrite(inPins[i],HIGH);
+ }
+ //set all the appropriate pins to output
+ for (int i = 0; i < 7; ++i) {
+    pinMode(outPinsStandard[i],OUTPUT);
+ }
+   pinMode(switchPin,INPUT);
+   digitalWrite(switchPin,LOW);
+}
+
+
+void loop()
+{
+  //check each pin
+  for (int i = 0; i < 8; ++i) {
+    //Checks the state of the current pin
+    pinState = digitalRead(inPins[i]); 
+    //checks if the switch is flipped   
+    if (digitalRead(switchPin)){
+      //if the switch is flipped the goofy output pin is written
+      digitalWrite(outPinsGoofy[i],pinState);
+    } else {
+      //if the switch is not flipped the standard output pin is written
+      digitalWrite(outPinsStandard[i],pinState);
+    }
+  }
+}
